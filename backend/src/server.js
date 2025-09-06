17771c901b70b6725b5fde357fb5eba1e8f131fb
Asif <asifd@gmail.com>
2025-09-06 12:23:40 +0530

    stream & sentry added , vercel moved to backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,31 @@
+import "../instrument.mjs"; // Must be the first import
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { inngest, functions } from "./config/inngest.js";
 import { serve } from "inngest/express";
+import chatRoutes from "./routes/chat.route.js";
+
+import * as Sentry from "@sentry/node";
 
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(clerkMiddleware()); //req auth will be available in the request object
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.get("/debug-sentry", (req, res) => {
+  throw new Error("My first Sentry error!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello world 123");
 });
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+
+Sentry.setupExpressErrorHandler(app); // Sentry error handler
 
 const startServer = async () => {
   try {
